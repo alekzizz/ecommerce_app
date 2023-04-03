48389deacdbaaf3651fe80f6ce177d0125c3cdaf
aziuzia <aziuzia@grupazpr.pl>
2023-04-03 19:18:20 +0200

    navigation init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Home from "./routes/home/home.component";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
+
+const Navigation = () => (
+  <div>
+    <h1>I am navigation bar</h1>
+    <Outlet />
+  </div>
+);
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/home/" element={<Home/>}/>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
-  )
+  );
 };
 
 export default App;
