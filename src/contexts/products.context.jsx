a4d251f42c135552b7bdb07466c757f6477e75e7
aziuzia <aziuzia@grupazpr.pl>
2023-04-16 11:43:21 +0200

    populate firebase

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,13 +1,13 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
-import SHOP_DATA from "../shop-data.json";
+import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(SHOP_DATA);
+  const [products, setProducts] = useState([]);
   const value = { products, setProducts };
 
   return (
