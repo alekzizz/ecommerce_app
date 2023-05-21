7149346ce11baeece92e00a43cedf9b74aecec19
aziuzia <aziuzia@grupazpr.pl>
2023-05-21 20:13:43 +0200

    cart icon as styled component

--- src/components/cart-icon/cart-icon.components.jsx
+++ src/components/cart-icon/cart-icon.components.jsx
@@ -1,14 +1,13 @@
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import {
   selectCartCount,
   selectIsCartOpen,
 } from "../../store/cart/cart.selector";
 import { setIsCartOpen } from "../../store/cart/cart.action";
 
-import "./cart-icon.styles.scss";
+import { ShoppingIcon, CartIconContainer, ItemCount } from "./cart-icon.styles";
 
 const CartIcon = () => {
   const dispatch = useDispatch();
@@ -19,10 +18,10 @@ const CartIcon = () => {
   const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
 
   return (
-    <div className="cart-icon-container" onClick={toggleIsCartOpen}>
-      <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{cartCount}</span>
-    </div>
+    <CartIconContainer onClick={toggleIsCartOpen}>
+      <ShoppingIcon />
+      <ItemCount>{cartCount}</ItemCount>
+    </CartIconContainer>
   );
 };
 
