308cf60bde32885e56510825c04fccbd346bc2e6
aziuzia <aziuzia@grupazpr.pl>
2023-05-21 20:22:54 +0200

    directory item as styled component

--- src/components/cart-icon/cart-icon.styles.jsx
+++ src/components/cart-icon/cart-icon.styles.jsx
@@ -5,6 +5,7 @@ export const ShoppingIcon = styled(ShoppingSvg)`
   width: 24px;
   height: 24px;
 `;
+
 export const CartIconContainer = styled.div`
   width: 45px;
   height: 45px;
@@ -14,6 +15,7 @@ export const CartIconContainer = styled.div`
   justify-content: center;
   cursor: pointer;
 `;
+
 export const ItemCount = styled.span`
   position: absolute;
   font-size: 10px;
